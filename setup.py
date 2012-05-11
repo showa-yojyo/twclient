33dd9e8aa6d32b2f4e9d5aabf455e41c51c82f9d
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-05-11 13:24:39 +0000

    * MANIFEST.in: setup.py のために追加。 * README.txt: 配布用に追加。 * setup.py: sdist がましになるように修正。
    git-svn-id: svn://localhost/twclient/trunk@16 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ from glob import glob
 data_files = [("Microsoft.VC90.CRT", 
                ["Microsoft.VC90.CRT.manifest"] + glob(r'msvc*90.dll'))]
 
-setup(windows=['twclient.pyw'],
-      options={"py2exe":{"includes":["sip"]}},
-      data_files=data_files)
+setup(
+    name='twclient',
+    version='1.0',
+    description=u'A very simple Twitter client',
+    author='yojyo@hotmail.com',
+    author_email='yojyo@hotmail.com',
+    url='http://www.geocities.jp/showa_yojyo/download/',
+    windows=['twclient.pyw'],
+    options={"py2exe":{"includes":["sip"]}},
+    data_files=data_files)
