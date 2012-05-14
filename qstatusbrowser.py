96181fa0e76ee89beb5b335b574ceb0928ce4f4b
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-05-14 11:56:09 +0000

    * qstatusbrowser.py: os.path.expanduser はまだ使わないので、コードを修正。
    git-svn-id: svn://localhost/twclient/trunk@21 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- qstatusbrowser.py
+++ qstatusbrowser.py
@@ -12,14 +12,15 @@ class QStatusBrowser(QtGui.QTextBrowser):
     def loadResource(self, type, name):
         url = unicode(name.toString())
         if url.startswith('http://'):
-            dn = os.path.expanduser('./cache/')
+            #dn = os.path.expanduser('./cache/')
+            dn = './cache'
             if not os.path.isdir(dn):
                 os.mkdir(dn)
 
             fn = os.path.join(dn, hashlib.md5(url).hexdigest())
             if not os.path.isfile(fn):
                 urllib.urlretrieve(url, fn)
-                return QtGui.QTextBrowser.loadResource(self, type, QtCore.QUrl(fn))
+                return super(QStatusBrowser, self).loadResource(type, QtCore.QUrl(fn))
         else:
-            return QtGui.QTextBrowser.loadResource(self, type, name)
+            return super(QStatusBrowser, self).loadResource(type, name)
 
