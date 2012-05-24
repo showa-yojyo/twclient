f8a0e3647828badc40197417a810904098e4c64f
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-05-24 15:09:49 +0000

    * deploy.py: 管理用スクリプト追加。 * MANIFEST.in: 上記ファイル追加に伴う修正。
    git-svn-id: svn://localhost/twclient/trunk@35 6d5f733e-3d27-5e49-bc6b-6486813682f8

---
+++ deploy.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import shutil
+import zipfile
+
+DEST = r'D:\Dropbox\Public'
+
+SDIST_OUTPUT = 'dist/twclient-0.0.0.zip'
+PY2EXE_OUTPUT = 'twclient-0.0.0.win32'
+PY2EXE_OUTPUT_ZIP = PY2EXE_OUTPUT + '.zip'
+
+def main():
+    # src
+    shutil.copy2(SDIST_OUTPUT, DEST)
+
+    # win32
+    fout = zipfile.ZipFile(PY2EXE_OUTPUT_ZIP, 'w', zipfile.ZIP_DEFLATED)
+    for root, dirs, files in os.walk(PY2EXE_OUTPUT):
+        for file in files:
+            p = os.path.join(root, file)
+            fout.write(p, p)
+    fout.close()
+
+    shutil.copy2(PY2EXE_OUTPUT_ZIP, DEST)
+    os.remove(PY2EXE_OUTPUT_ZIP)
+
+if __name__ == '__main__':
+    main()
