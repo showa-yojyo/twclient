d1391f8638bb8770875b8f0e219e2f5d85472e0d
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-05-27 15:08:38 +0000

    * setup.py: py2exe かどうかのテストを一ヶ所にまとめた。
    git-svn-id: svn://localhost/twclient/trunk@38 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- setup.py
+++ setup.py
@@ -10,16 +10,16 @@ import py2exe
 from glob import glob
 import twversion
 
-def make_data_files():
+def make_data_files(use_py2exe):
     files = []
     files.append((".", ["README.txt", "timelines.ini.sample",]))
-    if sys.platform == 'win32' and sys.argv[-1] == 'py2exe':
+    if use_py2exe:
         files.append(("Microsoft.VC90.CRT", 
                       ["Microsoft.VC90.CRT.manifest"] + glob(r'msvc*90.dll')))
     return files
 
-def make_options():
-    if sys.platform == 'win32' and sys.argv[-1] == 'py2exe':
+def make_options(use_py2exe):
+    if use_py2exe:
         return {
             "py2exe":{
                 "dist_dir": 'twclient-{0}.win32'.format(twversion.VERSION),
@@ -29,40 +29,46 @@ def make_options():
     else:
         return {}
 
-options = make_options()
+def main():
+    use_py2exe = sys.platform == 'win32' and 'py2exe' in sys.argv
 
-setup(
-    name='twclient',
-    version='',
-    description=u'A very simple Twitter client',
-    author='yojyo@hotmail.com',
-    author_email='yojyo@hotmail.com',
-    url='http://www.geocities.jp/showa_yojyo/download/twclient.html',
-    windows=['twclient.pyw'],
-    options=options,
-    data_files=make_data_files())
+    options = make_options(use_py2exe)
+    
+    setup(
+        name='twclient',
+        version='',
+        description=u'A very simple Twitter client',
+        author='yojyo@hotmail.com',
+        author_email='yojyo@hotmail.com',
+        url='http://www.geocities.jp/showa_yojyo/download/twclient.html',
+        windows=['twclient.pyw'],
+        options=options,
+        data_files=make_data_files(use_py2exe))
 
-if sys.argv[-1] == 'py2exe':
-    import dateutil
-    import zipfile
+    if use_py2exe:
+        import dateutil
+        import zipfile
 
-    # Make sure the layout of dateutil hasn't changed
-    assert (dateutil.__file__.endswith('__init__.pyc') or
-            dateutil.__file__.endswith('__init__.py')), dateutil.__file__
+        # Make sure the layout of dateutil hasn't changed
+        assert (dateutil.__file__.endswith('__init__.pyc') or
+                dateutil.__file__.endswith('__init__.py')), dateutil.__file__
 
-    # D:\Python26\lib\site-packages\dateutil\zoneinfo
-    zoneinfo_dir = os.path.join(os.path.dirname(dateutil.__file__), 'zoneinfo')
+        # D:\Python26\lib\site-packages\dateutil\zoneinfo
+        zoneinfo_dir = os.path.join(os.path.dirname(dateutil.__file__), 'zoneinfo')
 
-    # D:\Python26\lib\site-packages
-    disk_basedir = os.path.dirname(os.path.dirname(dateutil.__file__))
-    zipfile_path = os.path.join(options['py2exe']['dist_dir'], 'library.zip')
-    z = zipfile.ZipFile(zipfile_path, 'a')
+        # D:\Python26\lib\site-packages
+        disk_basedir = os.path.dirname(os.path.dirname(dateutil.__file__))
+        zipfile_path = os.path.join(options['py2exe']['dist_dir'], 'library.zip')
+        z = zipfile.ZipFile(zipfile_path, 'a')
 
-    for absdir, directories, filenames in os.walk(zoneinfo_dir):
-        assert absdir.startswith(disk_basedir), (absdir, disk_basedir)
-        zip_dir = absdir[len(disk_basedir):]
-        for f in filenames:
-            if f.endswith('.tar.gz'):
-                z.write(os.path.join(absdir, f), os.path.join(zip_dir, f))
+        for absdir, directories, filenames in os.walk(zoneinfo_dir):
+            assert absdir.startswith(disk_basedir), (absdir, disk_basedir)
+            zip_dir = absdir[len(disk_basedir):]
+            for f in filenames:
+                if f.endswith('.tar.gz'):
+                    z.write(os.path.join(absdir, f), os.path.join(zip_dir, f))
 
-    z.close()
+        z.close()
+
+if __name__ == '__main__':
+    main()
