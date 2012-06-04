994e30c7e85a8a082f2fef1d0e0f3f6ab2369d1c
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-06-04 12:58:51 +0000

    * twcommand/preference.py (execute): フォントダイアログを表示する仮コードを実装。
    git-svn-id: svn://localhost/twclient/trunk@57 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- twcommand/preference.py
+++ twcommand/preference.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from twcommand import CommandBase
+from PyQt4.QtGui import QFontDialog
 from PyQt4.QtGui import QMessageBox
 
 class Preference(CommandBase):
@@ -9,7 +10,9 @@ class Preference(CommandBase):
         self.parent = parent
 
     def execute(self):
-        QMessageBox.warning(
-            self.parent, 
-            u"アプリケーションの設定", 
-            u"工事中")
+        target = self.parent.ui.textBrowser
+        font, ok = QFontDialog.getFont(target)
+        if not ok:
+            return
+
+        target.setFont(font)
