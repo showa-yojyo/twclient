f2935d713c63b5aefd4589b5f0eb59651087bc52
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-06-20 16:30:22 +0000

    * twmodel/accountcollection.py: 追加。FollowedBy, Follows の基底クラス。 * twmodel/followed_by.py: 大幅軽量化に成功。 * twmodel/follows.py: 大幅軽量化に成功。
    git-svn-id: svn://localhost/twclient/trunk@100 6d5f733e-3d27-5e49-bc6b-6486813682f8

---
+++ twmodel/accountcollection.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from twmodel.collection import Collection
+from twitter import Twitter, NoAuth
+
+class AccountCollection(Collection):
+    """
+    This is the superclass of class Follows and FollowedBy.
+    """
+
+    def __init__(self, screen_name):
+        super(AccountCollection, self).__init__()
+        self.screen_name = screen_name
+        self.user_ids_rest = None
+
+    def update_page_info(self, response):
+        pass
+
+    def do_request(self, next_cursor):
+        auth = NoAuth()
+        api = Twitter(auth=auth)
+        if self.user_ids_rest:
+            # api.users.lookup のみで事足りる。
+            return self._request_users_lookup(api, self.user_ids)
+        else:
+            kwargs = dict(screen_name=self.screen_name)
+            if next_cursor > 0:
+                kwargs[u'next_cursor'] = next_cursor
+            else:
+                kwargs[u'next_cursor'] = -1
+
+            request = self.make_request_api(api)
+            res1 = request(**kwargs)
+            self.next_cursor = res1[u'next_cursor']
+            return self._request_users_lookup(api, res1['ids'])
+
+    def _request_users_lookup(self, api, user_ids):
+        num_users = len(user_ids)
+        if not num_users:
+            self.user_ids_rest = None
+            self.next_cursor = 0
+            return list()
+
+        if num_users < 100:
+            # 一回のリクエストですべてのユーザー情報が取得できる。
+            ids = ','.join([str(id) for id in user_ids])
+            res2 = api.users.lookup(user_id=ids, include_entities=0)
+            sorted_res = self._sort_users_lookup(user_ids, res2)
+
+            # 残りをクリア
+            self.user_ids_rest = None
+        else:
+            # 1. 最初の 100 ユーザーだけをスライスして利用する。
+            user_ids_first = user_ids[:100]
+            ids = ','.join([str(id) for id in user_ids_first])
+            res2 = api.users.lookup(user_id=ids, include_entities=0)
+            sorted_res = self._sort_users_lookup(user_ids_first, res2)
+
+            # 残りは次回のスクロールで利用するので、キープしておく。
+            self.user_ids_rest = user_ids[100:]
+
+        return sorted_res
+
+    def _sort_users_lookup(self, user_ids, response):
+        """
+        Sorts the response from api.users.lookup with api.followers.ids
+        or api.friends.ids
+        """
+        sorted_response = [None] * len(user_ids)
+        for user in response:
+            user_id = user[u'id']
+            i = user_ids.index(user_id)
+            sorted_response[i] = user
+
+        return sorted_response
--- twmodel/followed_by.py
+++ twmodel/followed_by.py
@@ -1,32 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from twmodel.collection import Collection
-from twitter import Twitter, NoAuth
+from twmodel.accountcollection import AccountCollection
 
-class FollowedBy(Collection):
-    def __init__(self, screen_name):
-        super(FollowedBy, self).__init__()
-        self.screen_name = screen_name
-
-    def update_page_info(self, response):
-        pass
-
-    def do_request(self, next_cursor):
-        auth = NoAuth()
-        api = Twitter(auth=auth)
-
-        kwargs = dict(screen_name=self.screen_name)
-        if next_cursor > 0:
-            kwargs[u'next_cursor'] = next_cursor
-        else:
-            kwargs[u'next_cursor'] = -1
-
-        res1 = api.followers.ids(**kwargs)
-        if len(res1['ids']):
-            ids = ','.join([str(id) for id in res1['ids']])
-            res2 = api.users.lookup(user_id=ids, include_entities=0)
-            self.next_cursor = res1[u'next_cursor']
-            return res2
-        else:
-            self.next_cursor = 0
-            return list()
+class FollowedBy(AccountCollection):
+    def make_request_api(self, api):
+        return api.followers.ids
