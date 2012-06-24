7cc1f279d14d84f47795ebb739c713fabfbb3380
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-06-24 15:24:03 +0000

    * twformat.py: displau_url を安全な形で復活。
    git-svn-id: svn://localhost/twclient/trunk@106 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- twformat.py
+++ twformat.py
@@ -122,7 +122,7 @@ def format_status(item):
 
 HOT_TEXT_FORMAT_HASHTAG = u'''<a href="chrome://hashtag/{text}" title="#{text}">#{text}</a>'''
 
-HOT_TEXT_FORMAT_URL = u'''<a href="{url}" title="{expanded_url}">{url}</a>'''
+HOT_TEXT_FORMAT_URL = u'''<a href="{url}" title="{expanded_url}">{display_url}</a>'''
 
 HOT_TEXT_FORMAT_USER = u'''<a href="chrome://user_mention/{screen_name}" title="クリックでポップアップメニュー表示">@{screen_name}</a>'''
 
@@ -142,6 +142,9 @@ def format_text(status_item):
     #   u'indices': [0, 20],
     #   u'url': u'http://t.co/zi4sB8iv'}]
     for url in entities['urls']:
+        if not u'display_url' in url:
+            url[u'display_url'] = url[u'url']
+
         rep = dict(indices=url['indices'],
                    pattern=HOT_TEXT_FORMAT_URL.format(**url))
         replacements.append(rep)
