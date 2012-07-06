a0062ee9078ae29cc6cceeba2ecf3c6527813f67
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-07-06 10:21:24 +0000

    * twsettings.py (createSettings): 当関数を外部から import 許可とした。
    git-svn-id: svn://localhost/twclient/trunk@118 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- twsettings.py
+++ twsettings.py
@@ -4,16 +4,16 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
 def restorePlacement(widget, regkey, pos=QPoint(200, 200), size=QSize(450, 450)):
-    settings = _createSettings()
+    settings = createSettings()
     pos = settings.value("{0}/pos".format(regkey), pos).toPoint()
     size = settings.value("{0}/size".format(regkey), size).toSize()
     widget.resize(size)
     widget.move(pos)
 
 def savePlacement(widget, regkey):
-    settings = _createSettings()
+    settings = createSettings()
     settings.setValue("{0}/pos".format(regkey), widget.pos())
     settings.setValue("{0}/size".format(regkey), widget.size())
 
-def _createSettings():
+def createSettings():
     return QSettings("prefab", "twclient")
