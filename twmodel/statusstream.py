ebe58ee6d7e381f68e63a696de176a820426356f
work <work@6d5f733e-3d27-5e49-bc6b-6486813682f8>
2012-07-15 16:01:14 +0000

    * twmodel/statusstream.py: Observer パターンを PyQt の signal/slot で実装し直した。
    git-svn-id: svn://localhost/twclient/trunk@126 6d5f733e-3d27-5e49-bc6b-6486813682f8

--- twmodel/statusstream.py
+++ twmodel/statusstream.py
@@ -3,25 +3,27 @@
 from PyQt4.QtCore import *
 
 class StatusStream(QObject):
+
+    nextPageLoaded = pyqtSignal(list)
+    latestPageLoaded = pyqtSignal(list)
+
     def __init__(self, parent=None):
         super(StatusStream, self).__init__(parent)
-        self.observers = []
 
     def add_observer(self, observer):
-        if not observer in self.observers:
-            self.observers.append(observer)
+        assert observer
+        self.nextPageLoaded.connect(observer.on_load_next_page)
+        self.latestPageLoaded.connect(observer.on_load_latest_page)
 
     def remove_observer(self, observer):
-        if observer in self.observers:
-            self.observers.remove(observer)
+        self.nextPageLoaded.disconnect(observer.on_load_next_page)
+        self.latestPageLoaded.disconnect(observer.on_load_latest_page)
 
     def notify_observers(self, response, fetch_older):
         if not response:
             return
 
         if fetch_older:
-            for observer in self.observers:
-                observer.on_load_next_page(response)
+            self.nextPageLoaded.emit(response)
         else:
-            for observer in self.observers:
-                observer.on_load_latest_page(response)
+            self.latestPageLoaded.emit(response)
